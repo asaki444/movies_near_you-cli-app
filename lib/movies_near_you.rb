f6e2e224cd71698e278d5467ed9bd2e12f0dec20
Aditi Green <asaki444@gmail.com>
2017-04-26 18:27:24 -0400

    test to see if file runs

---
+++ lib/movies_near_you.rb
@@ -0,0 +1,5 @@
+require "movies_near_you/version"
+
+module MoviesNearYou
+  # Your code goes here...
+end
---
+++ spec/movies_near_you_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe MoviesNearYou do
+  it "has a version number" do
+    expect(MoviesNearYou::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
